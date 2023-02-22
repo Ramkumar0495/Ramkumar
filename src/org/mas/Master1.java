27acafb66d6d165aa3e66ccce20249e292322564
Ramkumar <ramkumar0495@gmail.com>
2023-02-22 15:42:23 +0530

    login

---
+++ src/org/mas/Master1.java
@@ -0,0 +1,17 @@
+package org.mas;
+
+public class Master1 {
+	private void one() {
+		System.out.println("one");
+
+	}
+	private void two() {
+		System.out.println("two");
+		
+	}
+	private void three() {
+		System.out.println("three");
+		
+	}
+
+}
